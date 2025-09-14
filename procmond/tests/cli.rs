185613347516e8c4a5628286b73e859488cf6e04
UncleSp1d3r <unclesp1d3r@evilbitlabs.io>
2025-09-13 23:44:01 -0400

    Add procmond crate with simple entrypoint and CLI test

---
+++ procmond/tests/cli.rs
@@ -0,0 +1,11 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn prints_expected_greeting() {
+    let mut cmd = Command::cargo_bin("procmond").unwrap();
+    cmd.assert().success().stdout(predicate::str::contains(
+        "Hello from sentinel-lib to procmond!",
+    ));
+}
